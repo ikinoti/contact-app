2ec617c1ca4a90ac2c41b022dfe0e77b4828a6b6
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-09-29 15:02:23 +0300

    observable for our contact

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Contact } from '../Contact';
+import { CONTACTS } from '../mock-contacts';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ContactService {
+  constructor() {}
 
-  constructor() { }
+  getContacts(): Observable<Contact[]> {
+    const contacts = of(CONTACTS);
+    return contacts;
+  }
 }
