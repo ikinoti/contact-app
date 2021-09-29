95a6ee3ed29f44aff6a33255ffbc96e6da730566
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-09-29 22:29:40 +0300

    added an event emiter for delete method"

--- src/app/components/contact-item/contact-item.component.ts
+++ src/app/components/contact-item/contact-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Contact } from 'src/app/Contact';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
@@ -9,9 +9,14 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
 })
 export class ContactItemComponent implements OnInit {
   @Input() contact: any;
+  @Output() onDeletetask: EventEmitter<Contact> = new EventEmitter();
   faTimes = faTimes;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  onDelete(contact: any) {
+    this.onDeletetask.emit(contact);
+  }
 }
