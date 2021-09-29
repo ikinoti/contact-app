eeffed0ca0deb0debbf1cd3d64c4384d21ee8343
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-09-29 11:14:28 +0300

    mock contacts data

---
+++ src/app/mock-contacts.ts
@@ -0,0 +1,28 @@
+import { Contact } from './Contact';
+
+export const CONTACTS: Contact[] = [
+  {
+    id: 1,
+    name: 'kinoti',
+    contact: '0710120950',
+    email: 'isaiah.gitonga@student.moringaschool.com',
+    location: 'Kasarani',
+    meetPlace: 'ihub nairobi',
+  },
+  {
+    id: 2,
+    name: 'carol',
+    contact: '0705902101',
+    email: 'carol@gmail.com',
+    location: 'Kangemi',
+    meetPlace: 'Mombasa road',
+  },
+  {
+    id: 3,
+    name: 'Munyonti',
+    contact: '0769769769',
+    email: 'wamunyota@gmail.com',
+    location: 'Embu',
+    meetPlace: 'Embu',
+  },
+];
