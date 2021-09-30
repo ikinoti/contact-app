756d66c032ee80e178d5158ce1877f3b8fddb886
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-09-30 23:52:37 +0300

    deploy to github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { ContactItemComponent } from './components/contact-item/contact-item.component';
+import { AddContactComponent } from './components/add-contact/add-contact.component';
+import { LoginComponent } from './components/login/login.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+const appRoutes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'contact', component: ContactsComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ButtonComponent
+    ButtonComponent,
+    ContactsComponent,
+    ContactItemComponent,
+    AddContactComponent,
+    LoginComponent,
+    FooterComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UiService } from 'src/app/services/ui.service';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -12,7 +13,7 @@ export class HeaderComponent implements OnInit {
   showAddContact: any;
   subscription: any;
 
-  constructor(private uiService: UiService) {
+  constructor(private uiService: UiService, private router: Router) {
     this.subscription = this.uiService
       .onToggle()
       .subscribe((value) => (this.showAddContact = value));
@@ -24,5 +25,9 @@ export class HeaderComponent implements OnInit {
     this.uiService.toggleAddContact();
 
     // console.log('toggle');
+  }
+
+  hasRoute(route: string) {
+    return this.router.url === route;
   }
 }
