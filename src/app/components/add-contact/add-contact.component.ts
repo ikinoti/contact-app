81fcb9ee0a7f56634712b48eccd81a4fc85dfe26
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-09-30 12:28:49 +0300

    captured inputed data via two way data binding

--- src/app/components/add-contact/add-contact.component.ts
+++ src/app/components/add-contact/add-contact.component.ts
@@ -3,13 +3,39 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-add-contact',
   templateUrl: './add-contact.component.html',
-  styleUrls: ['./add-contact.component.css']
+  styleUrls: ['./add-contact.component.css'],
 })
 export class AddContactComponent implements OnInit {
+  name: any;
+  contact: any;
+  email: any;
+  location: any;
+  meetPlace: any;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit() {
+    if (!this.name) {
+      alert('Please add a name!');
+      return;
+    }
+
+    const newContact = {
+      name: this.name,
+      contact: this.contact,
+      email: this.email,
+      location: this.location,
+      meetPlace: this.meetPlace,
+    };
+
+    // todo - emit event
+
+    this.name = '';
+    this.contact = '';
+    this.email = '';
+    this.location = '';
+    this.meetPlace = '';
   }
-
 }
